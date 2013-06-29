8b4b60fee88619be727eba992dc1ec930f311f7b
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2013-06-29 08:08:16 +0000

    - Implemented access control filter

---
+++ trunk/Edge/Core/Filters/AccessControl.php
@@ -0,0 +1,51 @@
+<?php
+namespace Edge\Core\Filters;
+
+use Edge\Core\Edge,
+    Edge\Core\Http;
+
+/**
+ * Prep rocess filter that checks whether the current user
+ * has permission to invoke the requested method.
+ * The usual way to apply the filter is to create an instance method
+ * in the controller, which defines a mapping between the controller's methods
+ * and the permissions each method requires, along with the filter itself
+ * Basic example
+ *
+ *
+    protected function getAclMap(){
+        return array(
+            'index' => array('create user')
+        );
+    }
+
+    public function filters(){
+        return array_merge(parent::filters(), array(
+            array(
+                'Edge\Core\Filters\AccessControl',
+                "permissions" => $this->getAclMap()[Edge::app()->router->getAction()]
+            )
+        ));
+    }
+ *
+ *
+ * @package Edge\Core\Filters
+ */
+class AccessControl extends BaseFilter{
+
+    private $permissions;
+
+    public function __construct(array $attrs){
+        parent::__construct(array_key_exists('applyTo', $attrs)?$attrs['applyTo']:array("*"));
+        $this->permissions = $attrs['permissions'];
+    }
+
+    public function preProcess(Http\Response $response, Http\Request $request){
+        foreach($this->permissions as $perm){
+            if(!Edge::app()->user()->hasPrivilege($perm)){
+                $response->httpCode = 401;
+                $response->write();
+            }
+        }
+    }
+}
