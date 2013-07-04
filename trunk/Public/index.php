897464ecbec0ec5a895ddadf13bb96a8495197c1
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2013-07-04 08:39:27 +0000

    - Added Public directory where all static files will reside. This way Edge is completely protected from outside access

--- trunk/Public/index.php
+++ trunk/Public/index.php
@@ -1,16 +1,14 @@
 <?php
 //Register class loader
-require('Edge/Core/ClassLoader.php');
+require('../Edge/Core/ClassLoader.php');
 $loader = new ClassLoader();
 $loader->registerNamespaces(array(
-    'Edge' => __DIR__,
-    'Application' => __DIR__,
-    'Monolog' => __DIR__."/Edge/Core/Logger"
+    'Application' => $loader->basePath
 ));
 $loader->register();
 use Edge\Core\Edge;
 
-$webApp = new Edge(__DIR__."/Application/Config/config.php");
+$webApp = new Edge($loader->basePath."/Application/Config/config.php");
 $router = $webApp->getConfig('routerClass');
 $oRouter = new $router($webApp->getRoutes());
 $oRouter->invoke();
