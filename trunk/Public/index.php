005bf190704c9b786598a8e251eb14ed5fd0c909
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2013-07-06 15:17:56 +0000

    - Registered new class loader based on spl_autoload_extensions which in conjuction with PSR-0 boosted performance

--- trunk/Public/index.php
+++ trunk/Public/index.php
@@ -1,14 +1,7 @@
 <?php
-//Register class loader
-require('../Edge/Core/ClassLoader.php');
-$loader = new ClassLoader();
-$loader->registerNamespaces(array(
-    'Application' => $loader->basePath
-));
-$loader->register();
+require('../Edge/Core/Loader.php');
 use Edge\Core\Edge;
-
-$webApp = new Edge($loader->basePath."/Application/Config/config.php");
+$webApp = new Edge("Application/Config/config.php");
 $router = $webApp->getConfig('routerClass');
 $oRouter = new $router($webApp->getRoutes());
 $oRouter->invoke();
