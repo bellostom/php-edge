c28beac0de49926afa38d806191e6bf1b64f9963
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2013-07-08 10:03:15 +0000

    - Cleaned up code to provide a simple skeleton for checked out versions

--- trunk/Application/Config/routes.php
+++ trunk/Application/Config/routes.php
@@ -1,17 +1,7 @@
 <?php
 return array(
     'GET' => array(
-        '/' => array("Home", "index"),
-        '/page/action/:name/:id' => array("Home", "index"),
-        '/new/test/:id' => array("Home", "test"),
-        '/view/city/:id' => array("Home", "city"),
-        '/home/session' => array("Limited", "index")
+        '/' => array("Home", "index")
 
-    ),
-    'POST' => array(
-        '/rest/api/:id' => array('Home', 'post')
-    ),
-    '*' => array(
-        '/api/update/:id' => array("Home", "test")
     )
 );
