ef40ae31f4d604df08f38f0250174a94f9fe87d4
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2013-09-02 08:22:57 +0000

    - Simplified Loader.php by removing lines specific to Monolog  - Added custom autoloader for Monolog

--- trunk/Edge/Core/Logger/Monolog/EdgeLogger.php
+++ trunk/Edge/Core/Logger/Monolog/EdgeLogger.php
@@ -1,7 +1,16 @@
 <?php
-namespace Monolog;
+namespace Edge\Core\Logger\Monolog;
 
-class EdgeLogger extends Logger{
+require "Edge/Core/Logger/Monolog/Logger.php";
+
+/**
+ * Class EdgeLogger
+ * Extend Monolog\Logger in order to define new options
+ * and add a custom autoloader for discovering modules
+ * under the Monolog namespace
+ * @package Edge\Core\Logger\Monolog
+ */
+class EdgeLogger extends \Monolog\Logger{
 
     public function addRecord($level, $message, array $context = array()){
         if(is_array($message) || is_object($message)){
@@ -10,11 +19,22 @@ class EdgeLogger extends Logger{
         parent::addRecord($level, $message, $context);
     }
 
+    protected static function autoload(){
+        spl_autoload_register(function($class){
+            $file = "../Edge/Core/Logger/".str_replace('\\','/',$class).".php";
+            if(is_file($file)){
+                include $file;
+            }
+        });
+    }
+
     public static function factory(array $attrs){
+        static::autoload();
         $dateFormat = $attrs['dateFormat'];
         $output = "%datetime%: %level_name% - %message%\n";
-        $formatter = new Formatter\LineFormatter($output, $dateFormat);
-        $stream = new Handler\StreamHandler($attrs['file'], constant(__NAMESPACE__.'\Logger::'.$attrs['logLevel']));
+        $formatter = new \Monolog\Formatter\LineFormatter($output, $dateFormat);
+        $stream = new \Monolog\Handler\StreamHandler($attrs['file'],
+                                                    constant('\Monolog\Logger::'.$attrs['logLevel']));
         $stream->setFormatter($formatter);
         $log = new EdgeLogger('Edge');
         $log->pushHandler($stream);
