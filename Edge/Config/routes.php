0bda7ba53e65667acdbdf296d1350a67928d5bdd
bellosthomas@gmail.com <bellosthomas@gmail.com@1a6075f4-15e3-2092-103d-bbd90989ba58>
2014-03-07 14:38:02 +0000

    - Handle query string parameters in static urls

--- Edge/Config/routes.php
+++ Edge/Config/routes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'GET' => array(
-        '/js/:file' => array("Edge\\Controllers\\Asset", "js"),
-        '/css/:file' => array("Edge\\Controllers\\Asset", "css")
+        '/js/:file/*' => array("Edge\\Controllers\\Asset", "js"),
+        '/css/:file/*' => array("Edge\\Controllers\\Asset", "css")
     )
 );
