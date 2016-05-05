ec0f67a95521d4feed566c204c82321a66ae128c
bellos <bellos@intralot.com>
2016-05-05 11:34:59 +0300

    - Fixed php path in travis

--- tests/scripts/memcache.sh
+++ tests/scripts/memcache.sh
@@ -4,5 +4,5 @@ if [ $(phpenv version-name) = "7.0" ]; then
   echo "skipping memcache on php 7"
 else
   mkdir -p ~/.phpenv/versions/$(phpenv version-name)/etc
-  echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 fi
