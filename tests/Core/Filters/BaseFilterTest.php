a2199f6613d300b25b50bdba080905e27854571f
bellos <bellos@intralot.com>
2016-05-06 22:37:53 +0300

    - Added tests for BaseFilter

---
+++ tests/Core/Filters/BaseFilterTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Edge\Tests\Core\Filters;
+
+use Edge\Tests\EdgeTestCase,
+    Edge\Core\Filters\Authentication;
+
+class BaseFilterTest extends EdgeTestCase{
+
+    protected $filter;
+
+    protected function getFilter(array $params){
+        $params['url'] = "http://login/url";
+        return new Authentication($params);
+    }
+
+    public function testActionIncludedInApplyTo(){
+        $filter = $this->getFilter([
+            "applyTo" => ["method"]
+        ]);
+        $this->assertTrue($filter->appliesTo("method"));
+        $this->assertFalse($filter->appliesTo("login"));
+    }
+
+    public function testActionIncludedInApplyToDefault(){
+        $filter = $this->getFilter([]);
+        $this->assertTrue($filter->appliesTo("method"));
+        $this->assertTrue($filter->appliesTo("login"));
+    }
+
+    public function testActionExclusion(){
+        $filter = $this->getFilter([
+            "exceptions" => ["method"]
+                                   ]);
+        $this->assertFalse($filter->appliesTo("method"));
+        $this->assertTrue($filter->appliesTo("login"));
+    }
+}
