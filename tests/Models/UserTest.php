74227ebdc900771e0a4a7d7ce54ee6559fb82763
bellos <bellos@intralot.com>
2016-05-08 10:48:45 +0300

    - Added user testing

---
+++ tests/Models/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace Edge\Tests\Models;
+
+use Edge\Models\User;
+use Edge\Tests\EdgeWebTestCase;
+
+class UserTest extends EdgeWebTestCase{
+
+    protected $user;
+
+    public function setUp(){
+        parent::setUp();
+        $user = new User(["salt" => "qEytjqCxtg1"]);
+        $user->username = "thomas";
+        $user->name = "Thomas";
+        $user->pass = "thomas";
+        $user->email = "bellosthomas@gmail.com";
+        $this->user = $user;
+    }
+
+    public function tearDown(){
+        $this->user->delete();
+        parent::tearDown();
+    }
+
+    public function testAdd(){
+        $this->user->save();
+        $this->assertEquals($this->user->username, User::getUserByUsername("thomas")->username);
+    }
+
+    public function testGetByAttributes(){
+        $this->user->save();
+        $this->assertEquals($this->user->id, User::getUserById($this->user->id)->id);
+        $this->assertEquals($this->user->email, User::getUserByEmail($this->user->email)->email);
+        $this->assertTrue($this->user->authenticate("thomas"));
+        $this->assertFalse($this->user->hasPrivilege("delete user"));
+    }
+
+}
